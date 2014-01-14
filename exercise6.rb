eb67957471726c395fe81a0923b5f14afff741c4
nizarith <nizarith@live.com>
2014-01-14 14:32:45 -0330

    number and banana search

--- exercise6.rb
+++ exercise6.rb
@@ -1,13 +1,16 @@
-shopping_list = ["* chicken", "* rice", "* soy", "* curry"]
+shopping_list = ["* carrots", "* toilet paper", "* apples", "* salmon"]
 
 puts shopping_list
+# better way to list?
 
 puts "What else would you like to add to your shopping list?"
 
-item = "* " + gets.chomp.to_s
-
-shopping_list << item
-
+a = "* " + gets.chomp
+shopping_list << a
+# make this a method
 puts shopping_list
 
 puts "The number of items in this list is " + shopping_list.length.to_s
+
+bananafact = shopping_list.include?("bananas")
+puts "It is #{bananafact} that this list contains bananas."
