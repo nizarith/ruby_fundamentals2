83602dfd240a68fdd26901bd218d72fe334f48c8
nizarith <nizarith@live.com>
2014-01-14 14:48:40 -0330

    final w/ secong item, sorting, and delete

--- exercise6.rb
+++ exercise6.rb
@@ -14,3 +14,12 @@ puts "The number of items in this list is " + shopping_list.length.to_s
 
 bananafact = shopping_list.include?("bananas")
 puts "It is #{bananafact} that this list contains bananas."
+
+item2 = shopping_list[1]
+puts "The second item in this array is #{item2}"
+
+puts shopping_list.sort!
+
+puts "There is no salmon here, let's take it off our list."
+shopping_list.delete("* salmon")
+puts shopping_list
