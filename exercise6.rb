f584ede2273cc327a4cbe6c7b6836c9eeaecbf1c
nizarith <nizarith@live.com>
2014-01-14 13:35:15 -0330

    adding one item

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,11 @@
 shopping_list = ["* chicken", "* rice", "* soy", "* curry"]
 
 puts shopping_list
+
+puts "What else would you like to add to your shopping list?"
+
+item = "* " + gets.chomp.to_s
+
+shopping_list << item
+
+puts shopping_list
