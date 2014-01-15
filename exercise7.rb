46f7aa601e91b925b75de77a6d4cfa6d18daf571
nizarith <nizarith@live.com>
2014-01-15 17:24:58 -0330

    revision

--- exercise7.rb
+++ exercise7.rb
@@ -4,30 +4,31 @@ students = {
   :cohort3 => 22
 }
 
-students.each do |cohort, number|
-	puts "#{cohort}: #{number} students"
-end
+ students.each do |cohort, number|
+   puts "#{cohort}: #{number} students"
+ end
+
 
 students[:cohort4] = 43
 
 puts "After adding cohort 4..."
 
 students.each do |cohort, number|
-	puts "#{cohort}: #{number} students"
-end
+   puts "#{cohort}: #{number} students"
+ end
 
 puts "Increasing the class sizes by 5%"
 
 students.each {|k, v| students[k] = (v * 1.05).floor}
 
 students.each do |cohort, number|
-	puts "#{cohort}: #{number} students"
-end
+   puts "#{cohort}: #{number} students"
+ end
 
 puts "After deleting cohort 2...
 "
 students.tap { |ds| ds.delete(:cohort2) }
 
 students.each do |cohort, number|
-	puts "#{cohort}: #{number} students"
-end
+   puts "#{cohort}: #{number} students"
+ end
